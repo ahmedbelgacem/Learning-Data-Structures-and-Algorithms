035efd2063554e8d69d5db951b1cfc10dbbc02aa
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2022-06-07 16:14:28 +0100

    Time: 95 ms (17.69%), Space: 14.4 MB (85.37%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,6 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        forward = list("".join(filter(str.isalnum, s.lower())))
-        backward = forward.copy()
-        backward.reverse()
-        return backward == forward
+        begining = 0
+        end = len(s) - 1
+        while begining <= end:
+            while(not s[begining].isalnum() and begining < end) : begining += 1
+            while(not s[end].isalnum() and begining < end) : end -= 1
+            if s[begining].lower() != s[end].lower():
+                return False
+            begining += 1
+            end -=1
+        return True
