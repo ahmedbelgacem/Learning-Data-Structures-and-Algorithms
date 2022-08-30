34bb62bc468b40498f6518d852a972992962d7c3
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2022-08-30 14:04:53 +0100

    Time: 40 ms (73.02%), Space: 13.9 MB (15.85%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,19 +1,6 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        current = 0
-        c = needle[0]
-        idx = 0
-        while idx < len(haystack):
-            char = haystack[idx]
-            if char == c:
-                if current == len(needle) - 1:
-                    return(idx - len(needle) + 1)
-                current = current + 1
-                c = needle[current]
-            else:
-                if current != 0:
-                    idx = idx - current
-                    current = 0
-                    c = needle[0]
-            idx = idx + 1
-        return -1
+        for idx in range(len(haystack) - len(needle) + 1):
+            if haystack[idx : idx + len(needle)] == needle:
+                return idx
+        return(-1)
