e0d913d8ddaa8ee5b7f0797df601eab5fa4d13ca
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2022-08-30 13:16:13 +0100

    Time: 53 ms (34.74%), Space: 13.8 MB (65.60%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -3,5 +3,5 @@ class Solution:
         result = [[1] * (i + 1) for i in range(numRows)]
         for i in range(numRows):
             for j in range(1, i):
-                result[i][j] = result[i - 1][j - 1] + result[i -1][j]
+                result[i][j] = result[i - 1][j - 1] + result[i - 1][j]
         return result
