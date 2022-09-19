ab8aeb5c82b3b54f2cfe9e21aec5a40f5fca6be3
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2022-09-19 18:11:17 +0100

    Time: 408 ms (27.74%), Space: 14.2 MB (96.44%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -8,14 +8,14 @@ class Solution:
                 provinces += 1
                 visited.add(i)
             queue = []
-            for idx,adj in enumerate(isConnected[i]):
+            for idx, adj in enumerate(isConnected[i]):
                 if adj and idx not in visited:
                     queue.append(idx)
                     visited.add(idx)
             while queue:
                 j = queue.pop(0)
-                for idx,adj in enumerate(isConnected[j]):
-                    if adj and idx not in visited:      
+                for idx, adj in enumerate(isConnected[j]):
+                    if adj and idx not in visited:
                         queue.append(idx)
                         visited.add(idx)
         return provinces
