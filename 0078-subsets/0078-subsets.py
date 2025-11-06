698ac757b6898020f565564651f447e2c962800e
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2025-11-06 15:37:12 +0100

    Time: 0 ms (100%), Space: 17.9 MB (65.05%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,8 +1,12 @@
+import copy
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        
-        for num in nums:
-            res += [el + [num] for el in res]
-        
-        return res
+        if len(nums) == 1:
+            return [[], nums]
+        else:
+            tmp = self.subsets(nums[:-1])
+            res = copy.deepcopy(tmp)
+            for el in tmp:
+                el.append(nums[-1])
+                res.append(el)
+            return res
