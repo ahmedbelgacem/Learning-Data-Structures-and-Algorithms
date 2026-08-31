b0ad9209f5808a3d09d063feffc7e211e802471b
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2026-08-31 22:38:07 +0200

    Time: 0 ms (100%), Space: 19.2 MB (52.58%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,7 +1,3 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        s = str(num)
-        for i in range(len(s)):
-            if s[i] == '6':
-                return int(s[:i] + '9' + s[i+1:])
-        return num
+        return int(str(num).replace('6', '9', 1))
