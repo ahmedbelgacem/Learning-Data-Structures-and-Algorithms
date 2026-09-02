90e1fe7ceb18ec03a1d652eb96c7bb6f0d89adac
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2026-09-02 22:04:25 +0200

    Time: 0 ms (100%), Space: 19.2 MB (57.42%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.py
@@ -0,0 +1,7 @@
+class Solution:
+    def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
+        output = []
+        for element in order:
+            if element in friends:
+                output.append(element)
+        return output
